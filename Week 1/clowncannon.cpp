c1c8b7353b3fb709b159d715f2b880908ec76da2
David Thompson <davidbthomp@gmail.com>
2021-06-08 16:21:54 -0500

    CPP File setup

---
+++ Week 1/clowncannon.cpp
@@ -0,0 +1,23 @@
+/*
+* Yikes the clown cannon Assignment
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	// variables
+
+
+	// input
+
+
+	// processing
+
+	
+	// output
+
+}
---
+++ Week 1/clownskate.cpp
@@ -0,0 +1,23 @@
+/*
+* Yikes the clown skate Assignment
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	// variables
+
+
+	// input
+
+
+	// processing
+
+	
+	// output
+
+}
