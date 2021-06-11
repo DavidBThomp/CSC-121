21c0987c56006523f06f91835a2b1c8f43582077
David Thompson <davidbthomp@gmail.com>
2021-06-11 17:55:15 -0500

    Yikes Skate Completed

--- Week 1/clownskate.cpp
+++ Week 1/clownskate.cpp
@@ -1,5 +1,6 @@
 /*
 * Yikes the clown skate Assignment
+* Takes input of height of hill and height of ramp to calculate velocity
 */
 
 #include <iostream>
@@ -10,14 +11,21 @@ using namespace std;
 int main()
 {
 	// variables
-
+	double hill=0.0, ramp=0.0;
 
 	// input
-
+	cout << "Yikers the clown is skating downa hill, help his launch speed!\n";
+	cout << "Enter the height of hill in meters and the height of the ramp in meters to find out:";
+	cin >> hill >> ramp;
 
 	// processing
+	double heightMaths=hill-ramp;
+	double gravity=2.0 * 9.8;
+	double velocity=sqrt(gravity*heightMaths);
 
-	
+
 	// output
+	cout << "Yikes will be going " << velocity << " meters per second at the end of the ramp.\n";
+	return 0;
 
 }
