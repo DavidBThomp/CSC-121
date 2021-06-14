62619c1778f02c2f8c96bcca154afbca4db0eff5
David Thompson <davidbthomp@gmail.com>
2021-06-14 15:06:32 -0500

    Error Message Functional

--- Week 2/YikesRevisited.cpp
+++ Week 2/YikesRevisited.cpp
@@ -1,81 +1,36 @@
+/*
+* Yikes the clown cannon revisted assignment
+* Take input in MPH and angle in degrees and calculates landing
+*/
+
 #include <iostream>
 #include <cmath>
 
 using namespace std;
-const int SPHERE=1;
-const int CUBE=2;
-const int CYLINDER=3;
-const int QUIT=4;
 
 int main()
 {
-	int menuChoice=0;
+	// variables
+	double velocity=0.0, angle=0.0;
 
-	// print menu
-	cout << "1. Sphere\n2. Cube\n3. Cylinder\n4. Quit\nEnter choice: ";
-	cin >> menuChoice;
+	// input
+	cout << "Yikers the clown is being shot out a cannon, help him determine where he lands!\n";
+	cout << "Enter his velocity (Miles Per Hour) and angle (degrees):";
+	cin >> velocity >> angle;
 
-	while (menuChoice != QUIT) {
-	// Menu choice for sphere
-		if (menuChoice==SPHERE) {
-			double radius=0.0;
-			cout << "Enter radius in inches:";
-        		cin >> radius;
-			while (radius < 0.0) {
-				cout << "Error! Radius must be >= 0.0\n";
-				cout << "Enter radius in inches:";
-        			cin >> radius;
-			}
-			double volume=4.0 / 3.0 * M_PI * pow(radius,3.0);
-			cout << "Volume is " << volume << " cubic inches\n";
-		}
+	if (velocity < 0) {
+		cout << "The velocity must be greater than 0 miles per hour.\n";
+	} else if (angle < 0 || angle > 90) {
+		cout << "The angle must be between 0 and 90 degrees.\n";
+	} else {
+		// processing
+		double feetps=velocity * 1.4667;
+		double angles = angle*M_PI/180.0;
+		double distance=((feetps * feetps)/32.0) * sin(2.0*angles);
 
-		// Menu choice for cube
-		if (menuChoice==CUBE) {
-			double side=0.0;
-			cout << "Enter side in inches:";
-        		cin >> side;
-			while (side < 0.0) {
-				cout << "Error! Side must be >= 0.0\n";
-				cout << "Enter side in inches:";
-        			cin >> side;
-			}
-			double volume=pow(side,3.0);
-			cout << "Volume is " << volume << " cubic inches\n";
-		}
+		// output
+		cout << "Yikes will land " << distance << " feet from the cannon\n";
+		return 0;
+	}
 
-		if (menuChoice==CYLINDER) {
-			double height=0.0, radius=0.0, volume=0.0;
-			cout << "Enter height in inches:";
-			cin >> height;
-			while (height < 0.0) {
-				cout << "Error! Height must be >= 0.0\n";
-				cout << "Enter height in inches:";
-				cin >> height;
-			}
-
-			cout << "Enter radius in inches:";
-			cin >> radius; 
-			while (radius < 0.0) {
-				cout << "Error! Radius must be >= 0.0\n";
-				cout << "Enter radius in inches:";
-				cin >> radius;
-			}
-
-			volume=M_PI*pow(radius,2.0)*height;
-			cout << "Volume is " << volume << "cubic inches\n";
-		}
-
-		if (menuChoice != SPHERE && menuChoice != CUBE && menuChoice != CYLINDER) {
-			cout << "Error! Invalid menu choice\n";
-		}
-
-		// print menu
-		cout << "1. Sphere\n2. Cube\n3. Cylinder\n4. Quit\nEnter choice: ";
-		cin >> menuChoice;
-
-	} // while (menuChoice != QUIT)
-
-	cout << "Bye\n";
-
-	return 0;
+}
