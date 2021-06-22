82661750ce3e891e0d9a9295b8f4f63ac543fdd8
David Thompson <davidbthomp@gmail.com>
2021-06-22 16:26:51 -0500

    Attraction Inputs

--- Week 3/Attraction.cpp
+++ Week 3/Attraction.cpp
@@ -7,14 +7,15 @@
 #include <cmath>
 #include <climits>
 #include <string>
-#include "input.h"
 
 using namespace std;
         
         
 int main() {
-    double sr1=0.0, sr2=0.0, sm1=0.0, sm2=0.0;
-    cout << "Enter sphere's radius and then sphere's masses (Sphere 1 Radius, Sphere 2 Radius, Sphere 1 Mass, Sphere 2 Radius):";
+    double sr1=0.0, sr2=0.0, sm1=0.0, sm2=0.0, distance=0.0;
+
+    // Get Sphere Information
+    cout << "Enter sphere's radius and then sphere's masses in Meters and KGs(Sphere 1 Radius, Sphere 2 Radius, Sphere 1 Mass, Sphere 2 Radius):";
     cin >> sr1 >> sr2 >> sm1 >> sm2;
     while (cin.fail()==1) {
         cout << "\nError! Cannot read input\n";
@@ -28,7 +29,25 @@ int main() {
         cout << "Enter sphere's radius and then sphere's masses (Sphere 1 Radius, Sphere 2 Radius, Sphere 1 Mass, Sphere 2 Radius):";
         cin >>  sr1 >> sr2 >> sm1 >> sm2;
     }
-    double cubeSA=cubeSurfaceArea(cubeLen);
-    double cubeVOL=cubeVolume(cubeLen);
-    cout << "The surface area of the cube is " << cubeSA << " inches.\nThe volume of the cube is " << cubeVOL << " cubic inches.\n";
+
+    // Get Distance Information
+    cout << "Enter the distance between the two spheres in meters:";
+    cin >> distance;
+    while (cin.fail()==1) {
+        cout << "\nError! Cannot read input\n";
+        cin.clear();
+        cin.ignore(INT_MAX,'\n');
+        cout << "Enter the distance between the two spheres in meters:";
+        cin >>  distance;
+    }
+    while (distance < 0.0) {
+        cout << "Error! Length must be greater than 0.0 for all sides.\n";
+        cout << "Enter the distance between the two spheres in meters:";
+        cin >>  distance;
+    }
+
+    // Calculations
+    // double cubeSA=cubeSurfaceArea(cubeLen);
+    // double cubeVOL=cubeVolume(cubeLen);
+    cout << "sr1, sr2, sm1, and sm2 are all respectively " << sr1 << sr2 << sm1 << sm2<< ".\nThe distance is " << distance << " meters.\n";
 }
