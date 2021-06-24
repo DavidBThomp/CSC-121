6746812a8ab004eae6f4f3f2b431af830423767a
David Thompson <davidbthomp@gmail.com>
2021-06-24 18:53:22 -0500

    Final Touches

--- Week 3/ReadDouble.cpp
+++ Week 3/ReadDouble.cpp
@@ -20,69 +20,77 @@ using namespace std;
 
 void ReadChar(string &input) {
 
-    bool isValid=true;
-    isValid=true;
     string convert;
     string commas;
+    bool isValid=true;
     long len=input.length();
 
     long periodIndex = input.find(".");
     periodIndex = periodIndex + 1;
 
-    // Check that first input value is + - 0
-    if (input[0]!='+' && input[0]!='-' && isdigit(input[0]) == 0) {
-        std::cout << "Error! First digit input is not \"-\", \"+\", or \"0\".\n";
-        isValid=false;
-    } else {
-        // captures first charcter (+, -, 0)
-        convert=input.substr(0,1);
-    }
-
-    // check that the remaining characters are numeric
-    for (long index=1; index < len && isValid==true; index++) {
-        if (input[index]==',' || input[index]=='.') {
-            ; // Do nothing
+    do {
+        // Check that first input value is + - 0
+        if (input[0]!='+' && input[0]!='-' && isdigit(input[0]) == 0) {
+            std::cout << "Error! First digit input is not \"-\", \"+\", or \"0\".\n\n";
+            isValid=false;
+        } else {
+            // captures first charcter (+, -, 0)
+            convert=input.substr(0,1);
         }
-        else if (isdigit(input[index]) == 0){
-            //if the value is not a number
-            cout << "Error! Input was not an integer.\n";
+
+        // check that the remaining characters are numeric
+        for (long index=1; index < len; index++) {
+            if (input[index]==',' || input[index]=='.') {
+                ; // Do nothing
+            }
+            else if (isdigit(input[index]) == 0){
+                //if the value is not a number
+                cout << "Error! Input was not an integer.\n\n";
+                isValid=false;
+            }
+            else {
+                // adds first string back to validated input
+                convert += input.substr(index,1);
+            }
+        }
+
+        // Check all points after period
+        string decimalEnd;
+        decimalEnd += ".";
+        for (periodIndex; periodIndex < len; periodIndex++) {
+            if (input[periodIndex]==',' || input[periodIndex]=='.') {
+                //if values after period are period or comma
+                cout << "\n" <<"Error! Only one period is valid, and no periods or commas can come after initial period.\n\n";
+                isValid=false;
+            }
+
+            decimalEnd += input[periodIndex];
+
+        }
+
+
+        // Adds Commas before period where needed
+        long commaDec = input.find(".");
+        commaDec = commaDec;
+        commas = input.substr(0,commaDec);
+        long commasLen=commas.length();
+
+        long n = commasLen - 3;
+        while (n > 0) {
+            commas.insert(n, ",");
+            n-=3;
+        }  
+        
+
+        commas += decimalEnd;
+        
+        if (isValid==true) {
+            cout  << "The string input was " << commas << "\n\n";
+            isValid=false;
+        } else {
             isValid=false;
         }
-        else {
-            // adds first string back to validated input
-            convert += input.substr(index,1);
-        }
-    }
-
-    // Check all points after period
-    string decimalEnd;
-    decimalEnd += ".";
-    for (periodIndex; periodIndex < len && isValid==true; periodIndex++) {
-        if (input[periodIndex]==',' || input[periodIndex]=='.') {
-            //if values after period are period or comma
-            cout << "\n" <<"Error! Only one period is valid, and no periods or commas can come after initial period.\n";
-            isValid=false;
-        }
-
-        decimalEnd += input[periodIndex];
-
-    }
-
-    // Adds Commas before period where needed
-    long commaDec = input.find(".");
-    commaDec = commaDec;
-    commas = input.substr(0,commaDec);
-    long commasLen=commas.length();
-
-    long n = commasLen - 3;
-    while (n > 0) {
-        commas.insert(n, ",");
-        n-=3;
-    }  
-
-    commas += decimalEnd;
-    
-    cout  << "The string input was " << commas << "\n\n";
+    } while (isValid=false);
 
 }
 
