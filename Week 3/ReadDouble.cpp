4f002d81fbe405703f6c982903205fe6e500c0ad
David Thompson <davidbthomp@gmail.com>
2021-06-24 16:41:58 -0500

    All Valid, Start Checking Period Behavior

--- Week 3/ReadDouble.cpp
+++ Week 3/ReadDouble.cpp
@@ -1,8 +1,8 @@
 /*
 * ReadDouble Assignment
 * Take User Input as a String and read input into valid number
-* First charcter can be +, -, or decimal
-* rest of char can be numeric, comma, or decimal
+* First charcter can be +, -, or decimal            DONE
+* rest of char can be numeric, comma, or decimal        DONE
 * commas must be in proper location, before decimal, and seperating 3 digit groups
 * no commas after decimal point
 * only one decimal point
@@ -18,7 +18,52 @@ using namespace std;
 
 
 void ReadChar(string &input) {
+
+    bool isValid=true;
+    string convert;
+    
+    isValid=true;
+    
+
+    // Check that first input value is + - 0
+    if (input[0]!='+' && input[0]!='-' && isdigit(input[0]) == 0) {
+        std::cout << "Error! First digit input is not \"-\", \"+\", or \"0\".\n";
+        isValid=false;
+    } else {
+        // captures first charcter (+, -, 0)
+        convert=input.substr(0,1);
+    }
+
+    // check that the remaining characters are numeric
+    long len=input.length();
+    for (long index=1; index < len && isValid==true; index++) {
+        if (input[index]==',' || input[index]=='.') {
+            ;  // do nothing if character is a ','
+        }
+        else if (isdigit(input[index]) == 0){
+            //if the value is not a number
+            cout << "Error! Input was not an integer.\n";
+            isValid=false;
+        }
+        else {
+            // adds first string back to validated input
+            convert += input.substr(index,1);
+        }
+    }
+
+    // Checks period Behavior
+    int periodIndex;
+    for (long index=1; index < len && isValid==true; index++) {
+        if (input[index]=='.') {
+            periodIndex = index;
+        }
+    }
+    cout << periodIndex;
+    
+
     cout << "The string input was " << input << endl;
+
+
 }
 
 
