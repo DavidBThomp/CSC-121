cf77cbbdc7a2f353308c29403da4cae726f2145f
David Thompson <davidbthomp@gmail.com>
2021-07-02 12:13:23 -0500

    Quad Final

--- Week 4/Quad.cpp
+++ Week 4/Quad.cpp
@@ -24,20 +24,19 @@ int main()
     do {
 
         try {
-            done = true;
 
             // Get user input
-            cout << "Perform the Quadratic formula.\nPlease input A,B, and C in equation. (Ax^2 + Bx + C = 0):";
+            cout << "\nPerform the Quadratic formula.\nPlease input A,B, and C in equation. (Ax^2 + Bx + C = 0):";
             cin >> a >> b >> c;
 
             // Error Checking for inputs
             while (cin.fail()==1) {
-		        cout << "\nError! Cannot read input\n";
-		        cin.clear();
-		        cin.ignore(INT_MAX,'\n');
-		        cout << "Please input A,B, and C in equation. (Ax^2 + Bx + C = 0):";
+                cout << "\nError! Cannot read input\n";
+                cin.clear();
+                cin.ignore(INT_MAX,'\n');
+                cout << "Please input A,B, and C in equation. (Ax^2 + Bx + C = 0):";
                 cin >> a >> b >> c;
-	        }
+            }
 
             // Checking 4AC is less than b^2
             while ((4*a*c) > (b*b)) {
@@ -55,10 +54,22 @@ int main()
             // Output equation results
             cout << "X1 (positive equation) is equal to " << x1 << "\nX2 (negative equation) is equal to " << x2 << "\nX = " << x1 << ", " << x2 << ".\n";
 
+
         } catch (int e) {
             // Stores error in int e and then displays error.
             done=false;
             cerr << "An exception was thrown! Error number " << e << endl;
+        }
+
+
+        // Check to run again
+        string runAgain;
+        cout << "Would you like to run the program again? (Y)es or (N)o." << endl;
+        cin >> runAgain;
+        if (runAgain == "Y" || runAgain == "y") {
+            done = false;
+        } else {
+            done = true;
         }
 
     } while (done==false);
