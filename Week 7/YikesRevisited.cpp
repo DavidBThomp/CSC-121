dba845cf442aaf32a5d4160728de4e3164314c2a
David Thompson <davidbthomp@gmail.com>
2021-07-25 10:09:28 -0500

    Append and delete working

--- Week 7/YikesRevisited.cpp
+++ Week 7/YikesRevisited.cpp
@@ -10,10 +10,10 @@
 
 using namespace std;
 
-void maths(double velocity, double angle);
+void maths(double velocity, double angle, string fileName);
+void inputs(string fileName);
 
-int main()
-{
+int main() {
 
 	// File Opening
 	string fileName;
@@ -23,16 +23,34 @@ int main()
 	ifstream myfile(fileName);
 
 	double v, a;
+	char appendDelete;
 
 	try {
         if (myfile.is_open()) {
-            while (myfile >> v >> a){
+
+			while (myfile >> v >> a){
                 double velocity = v;
                 double angle = a;
 
-				maths(velocity, angle);
+				maths(velocity, angle, fileName);
             }
             myfile.close();
+
+
+			cout << "Would you like to (d)elete the file and start fresh, or (a)ppend and add the file:";
+			cin >> appendDelete;
+
+			if (appendDelete == 'd' || appendDelete == 'D') {
+				ofstream deleteFile;
+				deleteFile.open(fileName, ios::out);
+				deleteFile.close();
+				inputs(fileName);
+			} else if (appendDelete == 'a' || appendDelete == 'A') {
+				inputs(fileName);
+			} else {
+				cout << "Please use a valid input, either 'A' or 'D'." << endl;
+			}
+
         } else {
             cout << "Unable to find file." << endl << "Please enter a file name for data:";
 			cin >> fileName;
@@ -42,29 +60,11 @@ int main()
         cout << "Failure:" << e;
     }
 
-
-
-
-	// variables
-	double velocity=0.0, angle=0.0;
-
-	// input
-	cout << "Yikers the clown is being shot out a cannon, help him determine where he lands!\n";
-	cout << "Enter his velocity (Miles Per Hour) and angle (degrees):";
-	cin >> velocity >> angle;
-
-	if (velocity < 0) {
-		cout << "The velocity must be greater than 0 miles per hour.\n";
-	} else if (angle < 0 || angle > 90) {
-		cout << "The angle must be between 0 and 90 degrees.\n";
-	} else {
-		maths(velocity, angle);
-	}
 	return 0;
 
 }
 
-void maths(double velocity, double angle) {
+void maths(double velocity, double angle, string fileName) {
 	// processing
 	// Distance, Time, and Height
 	double feetps=velocity * 1.4667;
@@ -94,5 +94,42 @@ void maths(double velocity, double angle) {
 	// Expected output for 100MPH 30 Deg:
 	// Distance = 578.815~       Height = 83.545~    Time = 4.56~   
 	// Based from own calculations and quick rechecks from https://amesweb.info/Physics/Trajectory-Calculator.aspx
+
+	ofstream output;
+	output.open(fileName, ios::app);
+
+	if (output.is_open()==1) {
+	output.setf(ios::fixed | ios::right);
+	output << velocity << endl;
+	output << angle << endl;
+	output << "Yikes will land " << distance << " feet from the cannon." << endl;
+	output << "He will spend " << timeAir << " seconds in the air." << endl;
+	output << "His maxuimum height will be " << maxHeight << " feet." << endl;
+		if (output.fail()==1) {
+			cerr << "Error writing to file!\n";
+		}
+	}
+	output.close();
+
+
+
 }
 
+void inputs(string fileName) {
+	// variables
+	double velocity=0.0, angle=0.0;
+
+	// input
+	cout << "Yikers the clown is being shot out a cannon, help him determine where he lands!\n";
+	cout << "Enter his velocity (Miles Per Hour) and angle (degrees):";
+	cin >> velocity >> angle;
+
+	if (velocity < 0) {
+		cout << "The velocity must be greater than 0 miles per hour.\n";
+	} else if (angle < 0 || angle > 90) {
+		cout << "The angle must be between 0 and 90 degrees.\n";
+	} else {
+		maths(velocity, angle, fileName);
+	}
+
+}
